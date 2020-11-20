15df24b457c610d6907c0c4874818b730ed900c8
Nijun Wei <nijun.wei@topgene.cn>
2020-11-20 17:39:06 +0800

    提交结果完成界面

---
+++ pages/patient/patient-submit-sucess.vue
@@ -0,0 +1,46 @@
+<template>
+ 
+ <view>
+	 <view class="health-content">
+		 <view class="health-image"> <image class="health-status-image" src="../../static/icon/submit_sucess_icon.png"></image></view>
+		 <view class="health-result">提交成功</view>
+		 <button class="health-confirm">完成</button>
+	 </view>
+ </view>
+ 
+</template>
+
+<script>
+ 
+</script>
+
+
+<style lang="scss"> 
+
+.health-image {
+	text-align: center;
+	.health-status-image {
+		 margin-top: 80rpx;
+		 width: 170rpx;
+		 height: 194rpx;
+	}
+}
+ 
+ .health-result {
+	 // color: #333333;
+	 font-size: 32rpx;
+	 text-align: center;
+	 margin-top: 25rpx;
+ }
+ 
+ .health-confirm {
+	 top: 60rpx;
+	 width: 400rpx;
+	 height: 90rpx;
+	 font-size: 34rpx;
+	 color: white;
+	 background-color: #52A29E;
+	 border-radius: 45rpx;
+ }
+ 
+</style>
