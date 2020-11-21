bb661f9c31af937e9f7f88f35f3faa7f27e452b8
陈聪 <845991450@qq.com>
2020-11-21 17:49:54 +0800

    添加身份认证

---
+++ pages/authentication/detail.vue
@@ -0,0 +1,53 @@
+<template>
+	<view class="contentbox">
+		<view class="bigtitle">已认证</view>
+		<view class="flex">
+			<image src="../../static/mine/code.png" mode="aspectFill" class="imglist" @click="preview()"></image>
+			<image src="../../static/mine/code.png" mode="aspectFill" class="imglist" @click="preview()"></image>
+		</view>
+		<view class="changecss" @click="toindex">修改身份信息</view>
+		<view>备注：身份信息修改成功前，你的收益会继续入账修改前的账户。</view>
+	</view>
+</template>
+
+<script>
+	const app = getApp();
+	export default {
+		data() {
+			return {
+				baseUrl:app.globalData.baseUrl,
+				imgUrl:app.globalData.imageUrl,
+				
+			}
+		},
+		onLoad(options){
+			
+		},
+		onShow(){
+			
+		},
+		methods: {
+			preview(url){
+				uni.previewImage({
+					current:[],
+					urls:[]
+				})
+			},
+			toindex(){
+				uni.reLaunch({
+					url:"/pages/authentication/index"
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	.contentbox{padding:0 30rpx;
+		.bigtitle{font-size: 44rpx;line-height:142rpx;}
+		.imglist{width:50%;height:207rpx;border-radius:10rpx;box-sizing: border-box;
+			&:nth-child(1){margin-right:2%;width:48%;}
+		}
+		.changecss{color:#FF460A;padding:60rpx 0;}
+	}
+</style>
---
+++ pages/authentication/result.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="">
+		<view class="successbox">
+			<image class="successimg" mode="widthFix" src="../../static/end.png"></image>
+			<view class="keytext">信息已提交，等待平台审核</view>
+			
+		</view>
+		<view class="successbt" @click="tomine">完 成</view>
+	</view>
+</template>
+
+<script>
+	const app = getApp();
+	export default {
+		data() {
+			return {
+				imgUrl:app.globalData.imageUrl,
+				
+			}
+		},
+		onLoad(options){
+			
+		},
+		onShow(){
+			
+		},
+		methods: {
+			tomine(){
+				uni.reLaunch({
+					url:"/pages/mine/mine"
+				})
+			},
+		}
+	}
+</script>
+
+<style lang="scss">
+	.successbox{margin:150rpx 0 0;text-align:center;
+		.successimg{width:112rpx;height:auto;}
+		.keytext{font-size: 36rpx;margin:50rpx 0 0rpx;color:#272727;}
+	}
+	.successbt{line-height: 88rpx;background: #E4E4E4;border-radius: 10rpx;font-size: 38rpx;color: #2FAF36;text-align:center;position:fixed;bottom:120rpx;left:50%;margin-left:-180rpx;width:360rpx;}
+	page{border-top:1px solid #eee;}
+</style>
