4a1500659cd528523bd2a5954192c956fa172af9
Nijun Wei <nijun.wei@topgene.cn>
2020-11-23 14:29:38 +0800

    /.

--- App.vue
+++ App.vue
@@ -11,13 +11,14 @@
 			// AppSecret:"05ba4e0d30666a44d2c5be39fabf759d",
 		},
 		onLaunch: function(option) {
+			
 			console.log('App Launch');
-			// this.getupdate();
 			uni.baseImageUrl='https://bgjdapi-test.ttxyw.cn/upload/mini'
 			// //更新用户信息
 			// if(this.getCache("uid")) this.saveinfo();
 			const windowUrl = window.location.href;
 			const uid = this.getCache("uid") || option.uid;
+			console.log('uid=='+uid);
 			if(!uid){
 				window.location.href = this.globalData.baseUrl + '/wx/fwh/user/auth/index?returnUrl=' + encodeURIComponent(windowUrl);
 			}
@@ -33,27 +34,6 @@
 			...api,
 			getdata() {
 				console.log(this.globalData.mydata)
-			},
-			getupdate() {
-				//小程序自动更新
-				if (uni.canIUse('getUpdateManager')) {
-					const updateManager = uni.getUpdateManager()
-					updateManager.onCheckForUpdate(function(res) {})
-					updateManager.onUpdateReady(function() {
-						updateManager.applyUpdate()
-					})
-					updateManager.onUpdateFailed(function() {
-						uni.showModal({
-							title: '已经有新版本了哟~',
-							content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~',
-						})
-					})
-				} else {
-					uni.showModal({
-						title: '提示',
-						content: '当前版本过低，请升级到最新微信版本。'
-					})
-				}
 			},
 			setCacheAsync(key, data) {
 				//异步保存数据
