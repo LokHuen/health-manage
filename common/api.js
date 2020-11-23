61d649c2d78d5679b42f0676849818bf0048a1c7
陈聪 <845991450@qq.com>
2020-11-23 17:39:04 +0800

    修改身份认证页面

--- common/api.js
+++ common/api.js
@@ -9,6 +9,12 @@ var api = {
 	
 	//保存当前用户的身份认证
 	saveAuthentication:(data)=>{return rq("/wx/doctor/authentication/saveAuthentication",data,"POST");},
+	//所有省份列表
+	findProvinces:(data)=>{return rq("/wx/patient/findProvinces",data,"GET");},
+	//查询省份下城市
+	findCities:(data)=>{return rq("/wx/patient/findCities",data,"GET");},
+	//获取总行名称、编码
+	banklist:(data)=>{return rq("/wx/system/bank/list",data,"GET");},
 }
 
 export {api};
