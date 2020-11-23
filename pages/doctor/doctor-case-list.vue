92e1709854cfbfaec116bab2de056a14edbaa9f6
Nijun Wei <nijun.wei@topgene.cn>
2020-11-23 09:01:39 +0800

    医生营养管理病例界面

---
+++ pages/doctor/doctor-case-list.vue
@@ -0,0 +1,83 @@
+<template>
+	<!-- 医生营养管理病例界面 -->
+	<view class="container">
+		
+		<view class="pic-content-box" v-for="(item,indexItem) in itemList" :key="indexItem">
+			<view class="pic-time">2022/12/12 21:09 添加</view>
+			<view class="ccimglist">
+				<image v-for="(item,index) in list" :key="index" src="" mode="aspectFill" @click="previewImage(index)" :class="(index%3==0)?'imagelistfirst':'imagelist'"></image>
+			</view>
+		</view>
+		<view style="height: 100rpx;"></view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		components: {
+
+		},
+		data() {
+			return {
+				list: [1, 2, 3, 4, 5,6,7,8],
+				itemList:[1,2,3]
+			}
+		},
+		methods: {
+			previewImage(index) {
+				console.log(index);
+			},
+			addCase(){
+				
+			}
+		},
+		created() {
+
+
+
+		}
+
+	}
+</script>
+
+<style lang="scss">
+	.container {
+
+		
+
+		.pic-content-box {
+			.pic-time {
+				font-size: 15px;
+				color: #333333;
+				padding-top: 40rpx;
+				padding-left: 44rpx;
+			}
+
+			.ccimglist {
+				display: flex;
+				flex-wrap: wrap;
+				// justify-content:space-between;
+				margin-left: 44rpx;
+				margin-right: 44rpx;
+			   
+				.imagelistfirst {
+					margin-top: 20rpx;
+					width: 200rpx;
+					height: 200rpx;
+					background-color: #D2D2D2;
+				}
+				.imagelist {
+					margin-top: 20rpx;
+					width: 200rpx;
+					height: 200rpx;
+					background-color: #D2D2D2;
+					margin-left: 31rpx;
+				}
+			}
+
+
+		}
+
+	}
+</style>
--- unpackage/dist/dev/mp-weixin/common/runtime.js
+++ unpackage/dist/dev/mp-weixin/common/runtime.js
@@ -105,11 +105,11 @@
 /******/
 /******/
 /******/ 		// mini-css-extract-plugin CSS loading
-/******/ 		var cssChunks = {"components/uni-popup/uni-popup":1,"components/uni-transition/uni-transition":1};
+/******/ 		var cssChunks = {"components/uni-popup/uni-popup":1,"components/uni-icons/uni-icons":1,"components/uni-transition/uni-transition":1};
 /******/ 		if(installedCssChunks[chunkId]) promises.push(installedCssChunks[chunkId]);
 /******/ 		else if(installedCssChunks[chunkId] !== 0 && cssChunks[chunkId]) {
 /******/ 			promises.push(installedCssChunks[chunkId] = new Promise(function(resolve, reject) {
-/******/ 				var href = "" + ({"components/uni-popup/uni-popup":"components/uni-popup/uni-popup","components/stan-ucharts/LineChart":"components/stan-ucharts/LineChart","components/uni-transition/uni-transition":"components/uni-transition/uni-transition"}[chunkId]||chunkId) + ".wxss";
+/******/ 				var href = "" + ({"components/uni-popup/uni-popup":"components/uni-popup/uni-popup","components/uni-icons/uni-icons":"components/uni-icons/uni-icons","components/stan-ucharts/LineChart":"components/stan-ucharts/LineChart","components/uni-transition/uni-transition":"components/uni-transition/uni-transition"}[chunkId]||chunkId) + ".wxss";
 /******/ 				var fullhref = __webpack_require__.p + href;
 /******/ 				var existingLinkTags = document.getElementsByTagName("link");
 /******/ 				for(var i = 0; i < existingLinkTags.length; i++) {
