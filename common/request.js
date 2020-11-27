02e954cc0519074a8520386592688bcd0d864fea
陈聪 <845991450@qq.com>
2020-11-27 14:57:43 +0800

    bug

--- common/request.js
+++ common/request.js
@@ -1,4 +1,4 @@
-// var baseUrl = "http://weixinapi-test.cvddr.com";
+var baseUrl = "http://weixinapi-test.cvddr.com";
 
 function rq(url, data = {}, type, ctype) {
 	return new Promise((resolve, reject) => {
@@ -8,15 +8,15 @@ function rq(url, data = {}, type, ctype) {
 			method:type||"GET",
 		    header: {
 				'content-type': 'application/x-www-form-urlencoded',
-				//'uid':17
+				// 'uid':8
 		         'uid': localStorage.getItem("uid"),
-
 				// 'tkbgjdtoken':"ef56ZpDKDJ3qy/ttr+d4r16cTZd0ju+7Pz0eT/MtwDaIqSiojYJsYdyjGFLYy4/pJR4BSUmab7tIypyLTZtBYN/3obpPO4rLM6FyqNJEhp1LxjTgveFK/ls2Xr0/"
 				// 'tkbgjd-token':"634fw+Rth9zpdjXbKK9wEJcqlZZrzjvtudIxfIL6apurN02RT6Xn2mvhKBRqiD9cx0R2yWFv8njvE8w7FkGru5/LmmtHT0Vq+uWt"
 			},
 			success: (res) => {
 				if (res.data.status === -100) {
-					reject('未登录')
+					const windowUrl = window.location.href;
+					window.location.href = baseUrl + '/wx/fwh/user/auth/index?returnUrl=' + encodeURIComponent(windowUrl);
 				}
 				if (res.data.status !== 1) {
 					uni.showToast({
