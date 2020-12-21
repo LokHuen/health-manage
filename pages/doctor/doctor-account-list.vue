ec7cd38de78c36cac755a6ece1800ce5c5061856
陈聪 <845991450@qq.com>
2020-12-21 16:43:10 +0800

    文字

--- pages/doctor/doctor-account-list.vue
+++ pages/doctor/doctor-account-list.vue
@@ -18,7 +18,10 @@
 				<view class="received-yuan">元</view>
 			</view>
 			
-			<view class="received-detail">已结算金额</view>
+			<view class="received-detail">已结算金额
+				<view >当天转账的款项，在第二天（节假日顺延）入账银行账户</view>
+			</view>
+			
 		</view>
 		<view class="line"> </view>
 		
