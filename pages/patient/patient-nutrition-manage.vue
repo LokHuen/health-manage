7289b7b290819f5682651731ae6181b7d9aad8a8
陈聪 <845991450@qq.com>
2020-12-30 16:07:07 +0800

    修改

--- pages/patient/patient-nutrition-manage.vue
+++ pages/patient/patient-nutrition-manage.vue
@@ -835,7 +835,7 @@
 						left: '3%',
 						right: '3%',
 						bottom: '6%',
-						top: '5%',
+						top: '8%',
 						containLabel: true,
 					},
 					xAxis: {
@@ -1001,7 +1001,7 @@
 			.position2{transform: rotate(-28deg);left:6rpx;top:154rpx;}
 			.position3{transform: rotate(-20deg);left:0rpx;top:136rpx;}
 			.position4{transform: rotate(-10deg);left:-4rpx;top:118rpx;}
-			.position5{transform: rotate(-5deg);left:-4rpx;top:98rpx;}
+			.position5{transform: rotate(-5deg);left:-5rpx;top:100rpx;}
 			
 			//右下
 			.position25{transform: rotate(28deg);right:10rpx;top:172rpx;}
