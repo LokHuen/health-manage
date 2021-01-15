5f9a9ebbf4b42a1bd16e3e782cdf86c6f0aeac24
陈聪 <845991450@qq.com>
2021-01-15 15:29:40 +0800

    修改

--- pages/patient/patient-improve-msg.vue
+++ pages/patient/patient-improve-msg.vue
@@ -193,10 +193,10 @@
 	export default {
 
 		onLoad(props) {
+			if(!app.getCache("uid")) return;
 			this.getIllnessList();
 			this.type = props.type || 1; 
 			this.formQrCode = props.formQrCode ||2;
-			
 			http.get(http.urls.get_all_province).then((res) => {
 				this.areaList[0] = res.data;
 				if (this.areaList[0] && this.areaList[0].length > 0) {
