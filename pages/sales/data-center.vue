ffa40350af6ee4bbf95fd735c8da0a1540bba7bf
Nijun Wei <nijun.wei@topgene.cn>
2021-03-09 17:06:07 +0800

    数据中心界面

---
+++ pages/sales/data-center.vue
@@ -0,0 +1,252 @@
+<template>
+	<view class="container">
+		<!-- 总医生数 -->
+		<view class="item-box flex">
+			<view class="item-left">
+				<view class="item-number">215</view>
+				<view class="item-title">总医生数</view>
+			</view>
+			<view class="item-right">
+				<view class="item-right-detail flex" style="margin-top: 0rpx;">       
+					<view class="each1">维新生物</view>
+					<view class="each">200</view>
+					<view class="each1">本月新增</view>
+					<view class="each">18</view>
+				</view>
+				<view class="item-right-detail flex">
+                    <view class="each1">健康随我行</view>
+					<view class="each">10</view>
+					<view class="each1">本月新增</view>
+					<view class="each">18</view>
+				</view>
+				<view class="item-right-detail flex">
+					<view class="each1">海吉健康</view>
+					<view class="each">5</view>
+					<view class="each1">本月新增</view>
+					<view class="each">18</view>	
+				</view>
+				
+			</view>
+			
+		</view>
+		
+		<!-- 总患者数 -->
+		<view class="item-box flex">
+			<view class="item-left">
+				<view class="item-number">215</view>
+				<view class="item-title">总患者数</view>
+			</view>
+			<view class="item-right">
+				<view class="item-right-detail flex" style="margin-top: 0rpx;">       
+					<view class="each1">维新生物</view>
+					<view class="each">200</view>
+					<view class="each1">本月新增</view>
+					<view class="each">18</view>
+				</view>
+				<view class="item-right-detail flex">
+		            <view class="each1">健康随我行</view>
+					<view class="each">10</view>
+					<view class="each1">本月新增</view>
+					<view class="each">18</view>
+				</view>
+				<view class="item-right-detail flex">
+					<view class="each1">海吉健康</view>
+					<view class="each">5</view>
+					<view class="each1">本月新增</view>
+					<view class="each">18</view>	
+				</view>
+				
+			</view>
+			
+		</view>
+		
+		<!-- 总订单数 -->
+		<view style="background-color: #FFFFFF;">
+			<view class="item-box flex" style="height: auto;padding-top: 60rpx;">
+				<view class="item-left">
+					<view class="item-number">215</view>
+					<view class="item-title">总订单数</view>
+				</view>
+				<view class="item-right">
+					<view class="item-right-detail flex" style="margin-top: 0rpx;">       
+						<view class="each1">维新生物</view>
+						<view class="each">200</view>
+						<view class="each1">本月新增</view>
+						<view class="each">18</view>
+					</view>
+					<view class="item-right-detail flex">
+			            <view class="each1">健康随我行</view>
+						<view class="each">10</view>
+						<view class="each1">本月新增</view>
+						<view class="each">18</view>
+					</view>
+					<view class="item-right-detail flex">
+						<view class="each1">海吉健康</view>
+						<view class="each">5</view>
+						<view class="each1">本月新增</view>
+						<view class="each">18</view>	
+					</view>
+					
+				</view>
+				
+			</view>
+			<view class="tip">订单统计范围：3个小程序上客户已付款的所有商品订单数据</view>
+			<view class="order-tip">按商品统计订单数（3个小程序合计）</view>
+			
+			
+			<!-- item -->
+			<view style="margin-left: 50rpx;margin-right: 50rpx;background-color: #FFFFFF;">
+				<view class="item-box" style="background-color: #F6F6F6;padding-bottom: 60rpx;">
+					<view style="font-size: 28rpx;color: rgba(16, 16, 16, 100); text-align: left; margin-left: 50rpx; padding-top: 50rpx;">益爱宁</view>
+					<view class="flex" style="margin-top: 40rpx;">
+						<view class="item-left" style="width: 180rpx;">
+							<view class="item-number">215</view>
+							<view class="item-title">总订单数</view>
+						</view>
+						<view class="item-right" style="margin-left: 20rpx;margin-right: 20rpx;">
+							<view class="item-right-detail flex" style="margin-top: 0rpx;">       
+								<view class="each1">维新生物</view>
+								<view class="each">200</view>
+								<view class="each1">本月新增</view>
+								<view class="each">18</view>
+							</view>
+							<view class="item-right-detail flex">
+						        <view class="each1">健康随我行</view>
+								<view class="each">10</view>
+								<view class="each1">本月新增</view>
+								<view class="each">18</view>
+							</view>
+							<view class="item-right-detail flex">
+								<view class="each1">海吉健康</view>
+								<view class="each">5</view>
+								<view class="each1">本月新增</view>
+								<view class="each">18</view>	
+							</view>
+							
+						</view>
+					</view>
+					
+				</view>
+			</view>
+			
+			<!-- item -->
+			<view style="margin-left: 50rpx;margin-right: 50rpx;background-color: #FFFFFF;">
+				<view class="item-box" style="background-color: #F6F6F6;padding-bottom: 60rpx;">
+					<view style="font-size: 28rpx;color: rgba(16, 16, 16, 100); text-align: left; margin-left: 50rpx; padding-top: 50rpx;">陆点壹</view>
+					<view class="flex" style="margin-top: 40rpx;">
+						<view class="item-left" style="width: 180rpx;">
+							<view class="item-number">215</view>
+							<view class="item-title">总订单数</view>
+						</view>
+						<view class="item-right" style="margin-left: 20rpx;margin-right: 20rpx;">
+							<view class="item-right-detail flex" style="margin-top: 0rpx;">       
+								<view class="each1">维新生物</view>
+								<view class="each">200</view>
+								<view class="each1">本月新增</view>
+								<view class="each">18</view>
+							</view>
+							<view class="item-right-detail flex">
+						        <view class="each1">健康随我行</view>
+								<view class="each">10</view>
+								<view class="each1">本月新增</view>
+								<view class="each">18</view>
+							</view>
+							<view class="item-right-detail flex">
+								<view class="each1">海吉健康</view>
+								<view class="each">5</view>
+								<view class="each1">本月新增</view>
+								<view class="each">18</view>	
+							</view>
+							
+						</view>
+					</view>
+					
+				</view>
+			</view>
+			 <view style="height: 60rpx;"></view> 
+			
+		</view>
+	
+		
+		
+	</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	page {
+		background-color: $uni-bg-color-grey;
+	}
+
+	.container {
+		.item-box{
+			background-color: #FFFFFF;
+			margin-top: 26rpx;
+			height: 266rpx;
+			color: rgba(16, 16, 16, 100);
+			font-size: 24rpx;
+			font-family: PingFangSC-regular;
+			text-align: center;
+			.item-left{
+				margin-top: 0rpx;
+				height: 126rpx;
+				width: 240rpx;
+				border-right: 2rpx solid rgba(187, 187, 187, 100);
+				.item-number{
+					font-size: 36rpx;
+				}
+				.item-title{
+					margin-top: 28rpx;
+				}
+				
+			}
+			.item-right{
+				//text-align: left;
+				flex: 1;
+				margin-left: 40rpx;
+				margin-right: 40rpx;
+				//background-color: #4CD964;
+				.item-right-detail{
+					margin-top: 15rpx;
+					.each1{
+						width: 30%;
+					}
+					.each{
+						width: 20%;
+					}
+				}
+			}
+		}
+		.tip{
+			margin-top: 40rpx;
+			margin-left: 50rpx;
+			margin-right: 50rpx;
+			color: rgba(106, 105, 105, 100);
+			font-size: 24rpx;
+			text-align: center;
+			font-family: PingFangSC-regular;
+			border-bottom: solid 2rpx rgba(187, 187, 187, 100);
+			padding-bottom: 20rpx;
+		}
+		.order-tip{
+			margin-top: 32rpx;
+			margin-left: 50rpx;
+			color: rgba(16, 16, 16, 100);
+			font-size: 24rpx;
+			text-align: left;
+			font-family: PingFangSC-regular;
+		}
+	}
+</style>
