33da01409c5db48dcb50d376143baa815fcf50b7
陈聪 <845991450@qq.com>
2021-04-06 09:43:37 +0800

    bug

--- pages/sales/file-list.vue
+++ pages/sales/file-list.vue
@@ -106,6 +106,7 @@
 			   this.$refs.popup.open();
 			},
 			toDetail(item){
+				window.open(app1.globalData.baseUrl+item.fileUrl);
 				// var u = navigator.userAgent, app = navigator.appVersion;
 				// 				var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器 
 				// 				var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端 
@@ -120,9 +121,9 @@
 									 // var flieArr = item.fileName.split('.')
 									 // suffix = flieArr[flieArr.length - 1];
 				//       if(suffix=='pdf'){
-										 uni.navigateTo({
-										 	url:'file-detail?url='+item.fileUrl
-										 })	
+										 // uni.navigateTo({
+										 // 	url:'file-detail?url='+item.fileUrl
+										 // })	
 										 
 										//  window.open(app1.globalData.baseUrl+item.fileUrl,"_blank");
 										//  // var fileSrc = encodeURIComponent(app1.globalData.baseUrl + item.fileUrl);
