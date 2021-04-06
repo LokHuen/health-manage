3e79e854e4786aabf38de2fc3c97cbc9852d5e74
陈聪 <845991450@qq.com>
2021-04-06 10:21:25 +0800

    bug

--- pages/sales/file-list.vue
+++ pages/sales/file-list.vue
@@ -106,7 +106,7 @@
 			   this.$refs.popup.open();
 			},
 			toDetail(item){
-				window.open(app1.globalData.baseUrl+item.fileUrl);
+				window.open(app1.globalData.baseUrl+item.fileUrl,"_self");
 				// var u = navigator.userAgent, app = navigator.appVersion;
 				// 				var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //android终端或者uc浏览器 
 				// 				var isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端 
