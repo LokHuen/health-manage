e7d767c47cbe39c70cd6a6b68f05f990bba3e2e0
陈聪 <845991450@qq.com>
2021-04-14 11:31:51 +0800

    bug

--- components/tabbar.vue
+++ components/tabbar.vue
@@ -59,8 +59,8 @@
 					//数组元素有1个的时候，查到的是自己本身，就是普通的医生
 					//大于1的时候，表示主任
 					if(this.real) this.showCondition = res.data.resultList.length>1;
-					if(this.showCondition) localStorage.setItem("showCondition",1);
-					else localStorage.setItem("showCondition",0);
+					if(this.real&&this.showCondition) localStorage.setItem("showCondition",1);
+					if(this.real&&!this.showCondition) localStorage.setItem("showCondition",0);
 				}
 			});
 		},
