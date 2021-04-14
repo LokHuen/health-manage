1e7fb2b2de4df936e1fe0adaf0abc77a8cf42f08
陈聪 <845991450@qq.com>
2021-04-14 11:25:59 +0800

    bug

--- components/tabbar.vue
+++ components/tabbar.vue
@@ -53,11 +53,14 @@
 		},
 		created(){
 			if(!this.real) this.showCondition = true;
+			if(localStorage.getItem("showCondition")==1) this.showCondition = true;
 			app.allDoctorList().then(res =>{
 				if(res.status == 1){
 					//数组元素有1个的时候，查到的是自己本身，就是普通的医生
 					//大于1的时候，表示主任
 					if(this.real) this.showCondition = res.data.resultList.length>1;
+					if(this.showCondition) localStorage.setItem("showCondition",1);
+					else localStorage.setItem("showCondition",0);
 				}
 			});
 		},
