69c4c3a4092493401072bec0908eeb0e09b667e4
Nijun Wei <nijun.wei@topgene.cn>
2021-04-16 14:40:26 +0800

    新加消息提醒节目

---
+++ pages/doctor/message-remind.vue
@@ -0,0 +1,74 @@
+<template>
+	<!-- 消息提醒界面 -->
+	<view class="container">
+		<view class="item-box">
+			接收患者评估结果
+			<switch :checked="true" @change="switch1Change" color="#46918D" class="switch"/>
+		</view>
+		<view class="tip">
+			开启此功能，患者做了评估后，您会收到公众号推送的消息通知，能够及时知道患者每次评估的结果。
+		</view>
+		
+		<view class="item-box">
+			接收重度营养不良患者提醒消息
+			<switch :checked="true" @change="switch2Change" color="#46918D" class="switch"/>
+		</view>
+		<view class="tip">
+			开启此功能，当前一天有重度营养不良的患者时，您会收到提醒消息。
+		</view>
+	</view>
+</template>
+
+<script>
+	const app = getApp();
+	export default {
+		data() {
+			return {
+			   
+			}
+		},
+		onLoad(props){
+		    
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	page{
+		background-color: #F5F6F6;
+	}
+	.container{
+	    .item-box{
+			margin-top: 10rpx;
+			height: 110rpx;
+			line-height: 110rpx;
+			background: #FFFFFF;
+			padding-left: 50rpx;
+			font-size: 32rpx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #333333;
+			position: relative;
+			.switch{
+				position: absolute;
+				right: 50rpx;
+				width: 80rpx;
+				height: 48rpx;
+			}
+		}
+		.tip{
+			padding-left: 50rpx;
+			padding-right: 50rpx;
+			padding-top: 15rpx;
+			padding-bottom: 15rpx;
+			font-size: 24rpx;
+			font-family: PingFang SC;
+			font-weight: 400;
+			color: #999999;
+		}
+		
+	}
+</style>
