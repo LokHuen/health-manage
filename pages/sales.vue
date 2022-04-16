248d6aef281776b6fdff0b11f082dc12f3a3d065
陈聪 <845991450@qq.com>
2022-04-16 14:52:00 +0800

    修改

--- pages/sales.vue
+++ pages/sales.vue
@@ -13,7 +13,7 @@
 		data() {
 			return {
 				list:[
-					{name:"拓普健康业务",src:"http://weixingzh-test.cvddr.com/pages/sales/register"},
+					{name:"拓普健康业务",src:"http://weixingzh.cvddr.com/pages/sales/register"},
 				]
 			}
 		},
